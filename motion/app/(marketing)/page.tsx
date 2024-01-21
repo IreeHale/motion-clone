dcc521d940408152de325486e9e79bd570300a00
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-01-21 09:21:28 -0500

    working on market page of the project

---
+++ motion/app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+import {Medal} from "lucide-react";
+
+const MarketingPage = () => {
+    return (
+        <div className="flex items-center justify-center flex-col">
+           <div className="flex items-center justify-center flex-col">
+                <div>
+                    <Medal className="h-6 w-6 mr-2" />
+                </div>
+           </div>
+        </div>
+    );
+};
+
+export default MarketingPage;
