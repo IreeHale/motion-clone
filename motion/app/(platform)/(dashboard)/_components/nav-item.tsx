519a27dce1ec43663800a84da6ad45e364d507fa
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-01-29 09:55:11 -0500

    working workspace section of side bar

--- motion/app/(platform)/(dashboard)/_components/nav-item.tsx
+++ motion/app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+
 export type Organization = {
     id: string;
     slug: string;
@@ -22,8 +26,28 @@ export const NavItem = ({
     onExpand,
 }: NavItemProps) => {
     return (
-        <div>
-            Nav Item
-        </div>
+       <AccordionItem
+        value={organization.id}
+        className="border-none"
+       >
+        <AccordionTrigger
+            onClick={() => onExpand(organization.id)}
+            className={cn("flex items-center gap-x-2 p-1.5 text-neutral-700 rounded-md hover:bg-neutral-500/10 transition text-start no-underline hover:no-underline", 
+            isActive && !isExpanded && "bg-sky-500/10 text-sky-700")}
+        >
+
+            <div className="flex items-center gap-x-2">
+                <div className="w-7 h-7 relative">
+                    <Image 
+                     fill
+                     src={organization.imageUrl}
+                     alt="organization"
+                     className="rounded-sm object-cover"
+                    />
+                </div>
+            </div>
+            
+        </AccordionTrigger>
+       </AccordionItem>
     );
 };
