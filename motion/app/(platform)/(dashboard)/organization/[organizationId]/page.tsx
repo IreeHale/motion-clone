402dda91f9720820a558dccba614cc2c48a9ee0a
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-01-31 11:39:15 -0500

    working on the server section of the project

--- motion/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ motion/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,18 @@
+import { create } from "@/actions/create-boards";
+
 const OrganizationIdPage = () => {
+
     return (
         <div>
-          Organization Page!
+          <form action={create}>
+            <input 
+              id="title"
+              name="title"
+              placeholder="Enter a board title"
+              className="border-black border p-1"
+              required
+            />
+          </form>
         </div>
     );
 };
