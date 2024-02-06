1fd87b688f32f76f7bd8a6e3dd2b9872041f2015
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-02-06 11:01:17 -0500

    working on adding next parts of the project

--- motion/components/form/form-popover.tsx
+++ motion/components/form/form-popover.tsx
@@ -2,6 +2,7 @@
 
 import {
     Popover,
+    PopoverClose,
     PopoverContent,
     PopoverTrigger,
 } from "@/components/ui/popover";
@@ -9,6 +10,9 @@ import { useAction } from "@/hooks/use-action";
 import { createBoard } from "@/actions/create-board";
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { Button } from "../ui/button";
+import { X } from "lucide-react";
+import { toast } from "sonner";
 
 interface FormPopoverProps {
     children: React.ReactNode;
@@ -24,6 +28,23 @@ export const FormPopover = ({
     align,
     sideOffset = 0,
 }: FormPopoverProps) => {
+    const { execute, FieldErrors } = useAction(createBoard, {
+        onSuccess: (data) => {
+            console.log({ data });
+            toast.success("Board created!");
+        },
+        onError: (error) => {
+            console.log({ error });
+            toast.error(error);
+        }
+    });
+
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+
+        execute({ title });
+    }
+
     return (
         <Popover>
             <PopoverTrigger>
@@ -38,6 +59,24 @@ export const FormPopover = ({
                 <div className="text-sm font-medium text-center text-neutral-600 pb-4">
                     Create board
                 </div>
+                <PopoverClose asChild>
+                    <Button className="h-auto w-auto p-2 absolute top-2 right-2 text-neutral-600" variant="ghost" >
+                        <X className="h-4 w-4"/>
+                    </Button>
+                </PopoverClose>
+                <form action={onSubmit} className="space-y-4">
+                    <div className="space-y-4">
+                        <FormInput 
+                            id="title"
+                            label="Board title"
+                            type="text"
+                            errors={FieldErrors}
+                        />
+                    </div>
+                    <FormSubmit className="w-full">
+                        Create
+                    </FormSubmit>
+                </form>
             </PopoverContent>
         </Popover>
     );
