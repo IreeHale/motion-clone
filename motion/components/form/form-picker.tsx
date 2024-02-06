04b86f71dfd9763235cb8b83eb1e43e4c684e1e7
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-02-06 15:27:17 -0500

    adding form picker section

---
+++ motion/components/form/form-picker.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+interface FormPickerProps {
+    id: string;
+    errors?: Record<string, string[] | undefined>;
+};
+
+
+export const FormPicker = () => {
+    return (
+        <div>
+            Form Picker!
+        </div>
+    );
+};
