b082501b86e59531c26a97a8871b69329791a791
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-02-07 12:10:09 -0500

    first commit of the day

--- motion/components/form/form-picker.tsx
+++ motion/components/form/form-picker.tsx
@@ -1,12 +1,57 @@
 "use client"
 
+import { useEffect, useState } from "react";
+import { unsplash } from "@/lib/unsplash";
+import { Loader2 } from "lucide-react";
+
 interface FormPickerProps {
     id: string;
     errors?: Record<string, string[] | undefined>;
 };
 
 
-export const FormPicker = () => {
+export const FormPicker = ({
+    id,
+    errors,
+}: FormPickerProps) => {
+    const [images, setImages] = useState<Array<Record<string, any>>>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedImageId, setSelectedImageId] = useState(null);
+
+
+    useEffect(() => {
+        const fetchImages = async () => {
+            try{
+                const result = await unsplash.photos.getRandom({
+                    collectionIds: ["317099"],
+                    count: 9,
+                });
+
+                if (result && result.response) {
+                    const newImages = (result.response as Array<Record<string, any>>);
+                    setImages(newImages);
+                } else {
+                    console.log("Failed to get images from Unsplash");
+                }
+            } catch (error) {
+                console.log(error);
+                setImages([]);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchImages();
+    }, []);
+
+    if (isLoading) {
+        return (
+            <div className="p-6 flex items-center justify-center">
+                <Loader2  className="h-6 w-6 text-sky-700 animate-spin" />
+            </div>
+        )
+    }
+
     return (
         <div>
             Form Picker!
