34c93a0a35db35ad862bbbb622c3b7449ecfffc3
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-02-09 12:15:37 -0500

    making changes to popover unsplash image section

--- motion/components/form/form-picker.tsx
+++ motion/components/form/form-picker.tsx
@@ -2,11 +2,12 @@
 
 import { useEffect, useState } from "react";
 import { unsplash } from "@/lib/unsplash";
-import { Link, Loader2 } from "lucide-react";
+import { Check, Link, Loader2 } from "lucide-react";
 import { useFormStatus } from "react-dom";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { defaultImages } from "@/constants/images";
+import { FormErrors } from "./form-errors";
 
 interface FormPickerProps {
     id: string;
@@ -64,20 +65,32 @@ export const FormPicker = ({
                 {images.map((image) => (
                     <div
                         key={image.id}
-                        className={cn(
-                            "cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted",
-                            pending && "opacity-50 hover:opacity-50 cursor-auto")} onClick ={() => {
+                        className={cn("cursor-pointer relative aspect-video group hover:opacity-75 transition bg-muted", pending && "opacity-50 hover:opacity-50 cursor-auto")} onClick ={() => {
                                 if (pending) return;
                                 setSelectedImageId(image.id);
                             }}>
+                                <input
+                                    type="radio"
+                                    id={id}
+                                    name={id}
+                                    className="hidden"
+                                    checked={selectedImageId === image.id}
+                                    disabled={pending}
+                                    value={`${image.id}|${image.urls.thumb}|${image.urls.full}|${image.links.html}|${image.user.name}`}
+                                />
                                 <Image 
                                     src={image.urls.thumb}
                                     alt="Unsplash image"
                                     className="object-cover rounded-sm"
                                     fill
                                 />
+                                 {selectedImageId === image.id && (
+                                    <div className="absolute insert-y-0 h-full w-full bg-black/30 flex items-center justify-center">
+                                        <Check className="h-4 w-4 text-white"/>
+                                    </div>
+                                )}
                                 <Link 
-                                    href={image.link.html}
+                                    href={image.links.html}
                                     target="_blank"
                                     className="opacity-0 group-hover:opacity-100 absolute bottom-0 w-full text-[10px] truncate text-white hover: underline p-1 bg-black/50"
                                 >
@@ -86,6 +99,10 @@ export const FormPicker = ({
                             </div>
                 ))}
             </div>
+            <FormErrors 
+                id="image"
+                errors={errors}
+            />
         </div>
     );
 };
