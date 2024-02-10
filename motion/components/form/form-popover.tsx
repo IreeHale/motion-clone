4c3d7bfc87f1785ba7f78525aae397d635fb3363
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-02-10 16:34:32 -0500

    adding more fnctionality to images

--- motion/components/form/form-popover.tsx
+++ motion/components/form/form-popover.tsx
@@ -42,8 +42,11 @@ export const FormPopover = ({
 
     const onSubmit = (formData: FormData) => {
         const title = formData.get("title") as string;
+        const image = formData.get("image") as string;
 
-        execute({ title });
+        console.log({image});
+
+        //execute({ title });
     }
 
     return (
