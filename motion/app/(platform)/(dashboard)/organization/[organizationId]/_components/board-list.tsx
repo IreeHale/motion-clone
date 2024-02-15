3756c96fd3cfaae71373f3e1c480284a70f5419f
NightWing2099 <62215488+KidFlash2099@users.noreply.github.com>
2024-02-15 14:56:03 -0500

    working on board page

--- motion/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ motion/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -5,6 +5,7 @@ import { HelpCircle, Link, User2 } from "lucide-react";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export const BoardList = async () => {
     const { orgId } = auth();
@@ -61,6 +62,21 @@ export const BoardList = async () => {
                     </div>
                 </FormPopover>
             </div>
+        </div>
+    );
+};
+
+BoardList.Skeleton = function SkeletonBoardList() {
+    return (
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
+            <Skeleton className="aspect-video h-full w-full p-2" />
         </div>
     );
 };
